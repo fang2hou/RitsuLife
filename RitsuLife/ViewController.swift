16d38e7995f3ce094b040c296507097e43fa38f9
Fang2hou <houshuu@live.jp>
2017-03-09 21:06:27 +0900

    Initial Commit

---
+++ RitsuLife/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RitsuLife
+//
+//  Created by Fang Zhou on 2017/03/09.
+//  Copyright © 2017 Fang2hou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
